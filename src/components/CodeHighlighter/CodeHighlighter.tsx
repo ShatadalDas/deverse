cb7566e2c079adf7b4857f3de0389d3d17625c1b
ShatadalDas <shatadaldas2020@gmail.com>
2024-01-21 14:19:43 +0530

    chore: improve code highlighting styles and functionality

--- src/components/CodeHighlighter/CodeHighlighter.tsx
+++ src/components/CodeHighlighter/CodeHighlighter.tsx
@@ -1,33 +1,48 @@
-import styles from "./CodeHighlighter.module.scss"
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { vscDarkPlus as theme } from "react-syntax-highlighter/dist/esm/styles/prism"
-import languages from "@/utils/langArr"
-import { AllowArrayElementsOnly } from "@/utils/usefulGenericTypes"
+"use client";
 
-type LanguagesAllowed = AllowArrayElementsOnly<typeof languages>
+import "./CodeHighlighter.scss";    //? can't use css modules here
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
+import languages from "@/utils/langArr";
+import { AllowArrayElementsOnly } from "@/utils/usefulGenericTypes";
+import Image from "next/image";
+import { useState } from "react";
+import clipboardIcon from "../../assets/clipboard.png";
+import doneIcon from "../../assets/done.png";
+
+type LanguagesAllowed = AllowArrayElementsOnly<typeof languages>;
 
 type Props = {
-    code: string
-    lang: LanguagesAllowed
-    className?: string
-}
+  code: string;
+  lang: LanguagesAllowed;
+  className?: string;
+};
 
 function CodeHighlighter({ code, lang, className }: Props) {
-    return (
-        <>
-            <SyntaxHighlighter
-                language={ lang }
-                style={ theme }
-                showLineNumbers
-                wrapLines
-                className={ `${styles.codeBlock} ${className}` }
-            >
-                { code.trim() }
-            </SyntaxHighlighter>
-        </>
-    )
+  const [icon, setIcon] = useState(clipboardIcon);
+
+  function handleCopy() {
+    navigator.clipboard.writeText(code.trim());
+    setIcon(doneIcon);
+    setTimeout(() => {
+      setIcon(clipboardIcon);
+    }, 500);
+  }
+
+  return (
+    <div onClick={handleCopy} className="codeHighlightingWrapper">
+      <Image className="copyIcon" src={icon} alt="clipboard icon" />
+      <SyntaxHighlighter
+        language={lang}
+        style={theme}
+        showLineNumbers
+        wrapLines
+        className={`codeBlock ${className}`}
+      >
+        {code.trim()}
+      </SyntaxHighlighter>
+    </div>
+  );
 }
 
-export default CodeHighlighter
-
-
+export default CodeHighlighter;
