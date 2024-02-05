8d7baf187aa44d3a16496f8d570ac0009c259976
plaze <banikakash131@gamil.com>
2024-02-05 13:41:15 +0530

    feat(schema): add Post model

---
+++ src/backend/db/models/PostModel.ts
@@ -0,0 +1,29 @@
+import { model, models, Schema } from "mongoose";
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    upvotes: {
+      type: Number,
+      default: 0,
+    },
+    downvotes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default models.post || model("post", postSchema);
