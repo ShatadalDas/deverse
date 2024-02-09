d012d57936b5709ee215c8960f26984dadcc1b04
ShatadalDas <shatadaldas2020@gmail.com>
2024-02-09 10:14:24 +0530

    chore: correct semantic markup for post writing form

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -13,15 +13,17 @@ function Post() {
     <form action={onSubmit} className={styles.postForm}>
       <section className={styles.leftSection}>
         <div className={styles.descriptionWrapper}>
-          <p>Description</p>
+          <label htmlFor="post-description">Description</label>
           <textarea
+            id="post-description"
             className={styles.descriptionTextArea}
             placeholder="eg: Lorem ipsum dolor sit amet consectetur. Habitasse blandit nisl vitae duis rhoncus eu. Tellus quam sit id vulputate vitae et sed tellus. Posuere natoque in integer orci velit. Egestas pharetra eu morbi massa ut eu nisl consectetur."
           />
         </div>
         <div className={styles.hashtagsWrapper}>
-          <p>Hashtags</p>
+          <label htmlFor="hashtags-input">Hashtags</label>
           <textarea
+            id="hashtags-input"
             className={styles.hashtagsTextArea}
             placeholder="eg: #topic1  #topic2  #topic3"
           />
@@ -30,7 +32,8 @@ function Post() {
 
       <section className={styles.rightSection}>
         <div className={styles.buttonWrapper}>
-          <select defaultValue={"python"} className={styles.dropDownMenu}>
+          <label htmlFor="select-language" className="sr-only">Select Language for the code</label>
+          <select id="select-language" defaultValue={"python"} className={styles.dropDownMenu}>
             {langArr.map((lang) => (
               <option value={lang}>{lang}</option>
             ))}
@@ -38,8 +41,9 @@ function Post() {
           <GradientBtn text="Done" className={styles.gradientBtn} />
         </div>
         <div className={styles.codeWrapper}>
-          <p>Code</p>
+          <label htmlFor="code-input">Code</label>
           <textarea
+            id="code-input"
             className={styles.codeTextArea}
             placeholder="eg: print('Hello World')"
           />
